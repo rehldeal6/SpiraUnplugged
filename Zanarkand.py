cadaaaa0fcb1998a4684f1aae50d2bb7878fef8d
Jim Rehl <rehldeal6@gmail.com>
2019-10-04 18:34:27 -0400

    Initial Commit - Pretty much did configuration handling

---
+++ Zanarkand.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+#TODO: - Check to see if ffmpeg can start a video on a specific timestamp
+#      - Add in limited number of yt-dl requests
+#      - Add ability to auto-update
+#      - Set up proper logging
+import os
+import sys
+import time
+import ffmpeg
+import pprint
+import youtube_dl
+from itertools import cycle
+from argparse import ArgumentParser
+from multiprocessing import Process
+from ConfigParser import SafeConfigParser, NoSectionError, NoOptionError
+
+'''
+def count_to_fifteen():
+    for i in xrange(16):
+        print "{}".format(i)
+        time.sleep(1)
+    return
+
+def default_stream():
+    for i in xrange(100):
+        print "    Streaming default"
+        time.sleep(0.5)
+    return
+
+def test_default():
+    count = Process(target=count_to_fifteen)
+    count.start()
+    default = Process(target=default_stream)
+    default.start()
+    count.join()
+    default.terminate()
+'''
+
+def build_playlist(playlist_file):
+    game_dict = {}
+    game_list = []
+    highest_loop_position = 0
+
+    for s in playlist_file.sections():
+        positions = playlist_file.get(s, "LoopPositions")
+        for position in positions.split(","):
+            position = int(position)
+            if position > highest_loop_position:
+                highest_loop_position = position
+            if position in game_dict:
+                #log(LOG_ERR, "Duplicate loop position! {} and {} conflict".format(s, game_dict[position]))
+                sys.exit(4)
+            else:
+                game_dict[position] = s
+    for loop_position in xrange(1, highest_loop_position+1):
+        if loop_position not in game_dict:
+            print("Missing loop position! Position {} not found".format(loop_position))
+            #log(LOG_ERR, "Missing loop position! Position {} not found".format(loop_position))
+            sys.exit(4)
+        game_list.append(game_dict[loop_position])
+    return game_list
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--config", help="Main configuration file location", default="/etc/zanarkand/zanarkand.conf")
+    args = parser.parse_args()
+
+    zanarkand_defaults = {"FfmpegPreset": "superfast",
+                        "FfmpegCrf": 18,
+                        "FfmpegMinrate": "5000K",
+                        "FfmpegMaxrate": "6000K",
+                        "FfmpegBufsize": "12000K",
+                        "Resolution": 1080,
+                        "ViewportWidth": 1760,
+                        "ViewportHeight": 990,
+                        "ViewportX": 0,
+                        "ViewportY": 90,
+                        "OverlayImage": "/home/Zanarkand/resources/1080overlay.png",
+                        "PlaylistsFile": "/etc/zanarkand/playlist.conf",
+                        "CurrentStatusFile": "/etc/Zanarkand/current_status.txt",
+                        "DefaultVideoFile": "/etc/Zanarkand/default/default.v",
+                        "DefaultAudioFile": "/etc/Zanarkand/default/default.a",
+                        "LogFile": "/home/rehlj/ZanarkandPt2/test_log.log"}
+
+    zanarkand_config = SafeConfigParser(zanarkand_defaults)
+    try:
+        zanarkand_config.read(args.config)
+        #TODO - make this fail an check exception type
+    except Exception as e:
+        print("Could not open configuration file {}: {}".format(args.config, e))
+        #log(LOG_ERR, "Could not open configuration file {}".format(args.config))
+        sys.exit(1)
+    # Youtube Key
+    try:
+        zanarkand_config.get("youtube", "StreamKey")
+    except NoSectionError, NoOptionError:
+        print("No streamkey found. Needs to be in the {} file under a section named \"youtube\" with an option named \"StreamKey\"".format(args.config))
+        #log(LOG_ERR, "No streamkey found. Needs to be in the {} file under a section named \"youtube\" with an option named \"StreamKey\"".format(args.config))
+        sys.exit(2)
+
+    # ffmpeg Defaults
+    ffmpeg_preset = zanarkand_config.get("ffmpeg", "FfmpegPreset")
+    ffmpeg_crf = zanarkand_config.get("ffmpeg", "FfmpegCrf")
+    ffmpeg_min_rate = zanarkand_config.get("ffmpeg", "FfmpegMinrate")
+    ffmpeg_max_rate = zanarkand_config.get("ffmpeg", "FfmpegMaxrate")
+    ffmpeg_buffer_size = zanarkand_config.get("ffmpeg", "FfmpegBufsize")
+    ffmpeg_resolution = zanarkand_config.get("ffmpeg", "Resolution")
+    ffmpeg_viewport_width = zanarkand_config.get("ffmpeg", "ViewportWidth")
+    ffmpeg_viewport_height = zanarkand_config.get("ffmpeg", "ViewportHeight")
+    ffmpeg_viewport_x = zanarkand_config.get("ffmpeg", "ViewportX")
+    ffmpeg_viewport_y = zanarkand_config.get("ffmpeg", "ViewportY")
+    ffmpeg_overlay = zanarkand_config.get("ffmpeg", "OverlayImage")
+
+    # Stream Config
+    playlists = zanarkand_config.get("Zanarkand", "PlaylistsFile")
+    current_status = zanarkand_config.get("Zanarkand", "CurrentStatusFile")
+    default_video = zanarkand_config.get("Zanarkand", "DefaultVideoFile")
+    default_audio = zanarkand_config.get("Zanarkand", "DefaultAudioFile")
+    log_destination = zanarkand_config.get("Zanarkand", "LogFile")
+
+    # Check existance of files
+    config_files = [ffmpeg_overlay, playlists, default_video, default_audio]
+    found_files = True
+    for f in config_files: 
+        if not os.path.exists(f):
+            print("File {} not found".format(f))
+            #log(LOG_ERR, "File {} not found".format(f))
+            found_files = False
+    if not found_files:
+        sys.exit(3)
+
+    # Get games list
+    games_configs = SafeConfigParser()
+    try:
+        games_configs.read(playlists)
+    except Exception as e:
+        print("Could not open configuration file {}: {}".format(playlists, e))
+        #log(LOG_ERR, "Could not open configuration file {}".format(args.config))
+        sys.exit(1)
+
+    playlist_games = build_playlist(games_configs)
+    #pprint.pprint(playlist_games)
+
+    # Get current video, or make the file if it doesn't exist
+    if not os.path.exists(current_status):
+        with open(current_status, "w") as f:
+            f.write("[current]\n")
+            f.write("Game: {}\n".format(playlist_games[0]))
+            f.write("Episode: 1\n")
+            f.write("Loop: 1\n")
+            f.write("Position: 1\n")
+        game = playlist_games[0]
+        episode = 1
+        loop = 1
+        position = 1
+    else:
+        current_video = SafeConfigParser()
+        try:
+            current_video.read(current_status)
+            game = current_video.get("current", "Game")
+            episode = current_video.getint("current", "Episode")
+            loop = current_video.getint("current", "Loop")
+            position = current_video.getint("current", "Position")
+        except Exception as e:
+            print("Could not read from current status: {}".format(e))
+    
+    # Get current game and positon
+    game_cycle = cycle(playlist_games)
+    for _ in xrange(position):
+        current_game = game_cycle.next()
+    
+    print "current game: {}".format(current_game)
+
+
+if __name__ == "__main__":
+    main()
