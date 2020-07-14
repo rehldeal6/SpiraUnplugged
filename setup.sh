85e4d9c041e427b2788a6284dd6457d58044f848
Rehl <rehldeal6@users.noreply.github.com>
2020-07-14 12:36:09 -0400

    Update setup.sh
    Updating the link for the standby.

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@
      mkdir $(pwd)/resources
  fi
 
-youtube-dl https://www.youtube.com/watch?v=NhcsKAUBLb0 -o "$(pwd)/standby/standby.%(ext)s"
+youtube-dl https://www.youtube.com/watch?v=QtYienBNUAs -o "$(pwd)/standby/standby.%(ext)s"
 youtube-dl --playlist-items 135 https://www.youtube.com/playlist?list=PL9wpzJw8GKy74rLqQv7OH9v94Hj8qQWps -o "$(pwd)/standby/%(title)s"
 youtube-dl --playlist-items 136 https://www.youtube.com/playlist?list=PL9wpzJw8GKy74rLqQv7OH9v94Hj8qQWps -o "$(pwd)/standby/%(title)s"
 youtube-dl --playlist-items 137 https://www.youtube.com/playlist?list=PL9wpzJw8GKy74rLqQv7OH9v94Hj8qQWps -o "$(pwd)/standby/%(title)s"
